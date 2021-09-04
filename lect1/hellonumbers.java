4a436dd923a183e4c067097233a560cdfa16ba66
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-04 15:07:38 -0700

    Update hellonumbers.java

--- lect1/hellonumbers.java
+++ lect1/hellonumbers.java
@@ -1,9 +1,10 @@
-public class HelloNumbers{
-  public static void main(String[] args){
-    int x; /**declaration.*/
-    x=0;
-    while (x<10){
-      x=x+1;
+public class HelloNumbers {
+    public static void main(String args[]) {
+      int x;
+      x=0;
+      while(x<10){
+          System.out.println(x);
+          x=x+1;
+      }
     }
-  }
 }
