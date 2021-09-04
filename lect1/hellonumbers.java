9444a64af4268061ea369d7835b9d7668c501eb4
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-04 15:24:42 -0700

    Update hellonumbers.java

--- lect1/hellonumbers.java
+++ lect1/hellonumbers.java
@@ -1,10 +1,16 @@
 public class HelloNumbers {
     public static void main(String args[]) {
-      int x;
-      x=0;
-      while(x<10){
-          System.out.println(x);
-          x=x+1;
-      }
+        int x;
+        x=0;
+        while(x<10){
+            System.out.println(x);
+            x=x+1;
+        }
     }
 }
+/**All codes must be in a class.
+* Curly braces and semi-colons.
+* Variables have declared types and must be declared before use.
+* Functions must have a return type or declared as void function.
+* The compiler ensures type consistency.
+*/
