03bd7c2a8d00f0bf88b57613af86df08e7fa360a
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-04 16:02:13 -0700

    Update hellonumbers.java

--- lect1/hellonumbers.java
+++ lect1/hellonumbers.java
@@ -1,6 +1,6 @@
 public class HelloNumbers {
     public static void main(String args[]) {
-        int x;
+        int x;//1,2
         x=0;
         while(x<10){
             System.out.println(x);
@@ -8,3 +8,9 @@ public class HelloNumbers {
         }
     }
 }
+/**
+1. Before Java variables can be used, they must be declared.
+2. Java variables must have a specific type, like int.
+3. Java variable types can never change. Once it is a int, it's always an int.
+4. Types are varified before the codes even runs! If it fails to match, then all the codes won't run.
+*/
