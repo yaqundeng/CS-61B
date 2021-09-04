cae7fd0f7b31e044b4b933095d492042ba9776cc
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-04 15:26:24 -0700

    Update helloworld.java

--- lect1/helloworld.java
+++ lect1/helloworld.java
@@ -3,3 +3,10 @@ public class HelloWorld {
         System.out.println("Hello, World");
     }
 }
+/**
+1. All codes must be in a class.
+2. Curly braces and semi-colons.
+3. Variables have declared types and must be declared before use.
+4. Functions must have a return type or declared as void function.
+5. The compiler ensures type consistency.
+*/
