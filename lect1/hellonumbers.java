6800c4037cd607369de7cf0338b1b515abde7c26
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-04 15:26:10 -0700

    Update hellonumbers.java

--- lect1/hellonumbers.java
+++ lect1/hellonumbers.java
@@ -8,9 +8,3 @@ public class HelloNumbers {
         }
     }
 }
-/**All codes must be in a class.
-* Curly braces and semi-colons.
-* Variables have declared types and must be declared before use.
-* Functions must have a return type or declared as void function.
-* The compiler ensures type consistency.
-*/
