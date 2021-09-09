a4f8d1cb29e0e44ba3ecf58ef6a8d9704605360f
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-09 15:09:10 -0700

    Update Dog3.java

--- lect2/Dog3.java
+++ lect2/Dog3.java
@@ -1,27 +1,21 @@
 public class Dog{
     int WeightinPounds;
+    
+    public Dog(int w){
+        WeightinPounds=w;
+    }
 
-     public void makeNoise(){
-         if (WeightinPounds <10){
-             System.out.println("yip!");
+
+    public void makeNoise(){
+        if (WeightinPounds <10){
+            System.out.println("yip!");
          }
-         else if (WeightinPounds <30){
-             System.out.println("bark.");
+        else if (WeightinPounds <30){
+            System.out.println("bark.");
          }
-         else{
-             System.out.println("wooof!");
-         }
+        else{
+            System.out.println("wooof!");
+        }
         
      }
 }
-
-
-public class DogLaucher{
-    public static void main(String[] args){
-        Dog d = new Dog();
-        d.WeightinPounds =25;
-        d.makeNoise():
-    }
-}
-
-![image](https://user-images.githubusercontent.com/90111475/132766654-72b87bca-b57f-4915-85c2-90cf55b1a0d1.png)
