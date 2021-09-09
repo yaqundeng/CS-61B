65c63c079e83b68698d8c28ef7fae505696b6e1f
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-09 13:33:40 -0700

    Create Dog1.java

---
+++ lect2/Dog1.java
@@ -0,0 +1,10 @@
+public class Dog{
+
+     public static void makeNoise(){
+        System.out.println("bark");
+     }
+     
+     public static void main(String [] args){
+         makeNoise();
+     }
+}
