29f5c667c82fce20fd6eebd0736b014edfe7260e
yaqundeng <90111475+yaqundeng@users.noreply.github.com>
2021-09-09 18:10:05 -0700

    Update DogLauncher.java

--- lect2/DogLauncher.java
+++ lect2/DogLauncher.java
@@ -1,6 +1,11 @@
 public class DogLauncher{
-    public static void main (String[] args){
-        Dog d = new Dog(23);
-        d.makeNoise();
+
+    public static void main ( String [] args){
+            Dog d =new Dog(25);
+            Dog d2= new Dog(100);
+
+            Dog big=Dog.biggerdog(d,d2);
+            big.makenoise();
+            //d.makenoise();
     }
 }
